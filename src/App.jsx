28bbd1b59730a9b407e72d479e4b7c34892b3fde
faima-ahmed <faimaahmeddiya@gmail.com>
2025-01-01 16:12:26 +0600

    toolkit-apply

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CounterView from "./features/counter/CounterView";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <CounterView />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/features/counter/CounterView.jsx
@@ -0,0 +1,44 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount, reset } from "./counterSlice";
+
+const CounterView = () => {
+  const count = useSelector((state) => state?.counter?.count);
+  console.log(count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        Reset
+      </button>
+      <button
+        onClick={() => {
+          dispatch(incrementByAmount(5));
+        }}
+      >
+        IncrementByFive
+      </button>
+    </div>
+  );
+};
+
+export default CounterView;
